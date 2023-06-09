e93becfa76c6668aee1323cb4a6b6554736fd86c
Kim K <kimx582r@stud.kea.dk>
2023-06-09 17:37:01 +0200

    Initial Commit

---
+++ MyNotebook/MyNotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotebookApp.swift
+//  MyNotebook
+//
+//  Created by Kim Keiser on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyNotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
